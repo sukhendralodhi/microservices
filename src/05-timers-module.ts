8a004926b4d72a690dfd374950b2f3f780e756da
Sukhendra <developer.sukhendra@gmail.com>
2026-07-17 01:39:14 +0530

    promise based timer

--- src/05-timers-module.ts
+++ src/05-timers-module.ts
@@ -7,6 +7,8 @@
 // clearInterval
 // setImmediate
 
+import { setTimeout as sleep } from "node:timers/promises";
+
 function runSetTimeOutExample(): void {
     console.log("1. setTimeout example started");
 
@@ -25,9 +27,45 @@ function runClearTimeOutExample(): void {
     console.log("4. clearTimeout cancelled the 2 seconds timer");
 }
 
+// setInterval => it is a going to run a callback again and again after the fixed delay
+
+function runSetInterval(): void {
+    let count = 0;
+    const intervalId = setInterval(() => {
+        count++;
+        console.log("Setinterval tick", count);
+
+        if (count === 5) {
+            clearInterval(intervalId);
+            console.log("interval stopped");
+        }
+    }, 500);
+
+}
+
+// setImmediate => Use when you want to execute work as soon as the current operation finishes, without specifying a delay.
+
+function setImmediateExmaple(): void {
+    setImmediate(() => {
+        console.log("Request completed");
+    });
+
+    console.log("Synchronous code ");
+
+}
+
+async function runPromiseExmaple(): Promise<void> {
+    console.log("Waiting for promise based timer");
+    await sleep(5000);
+    console.log("Promise based timer finished");
+}
+
 function runTimerDemo(): void {
     runSetTimeOutExample();
     runClearTimeOutExample();
+    runSetInterval();
+    setImmediateExmaple();
+    runPromiseExmaple();
 }
 
 runTimerDemo();
