396cb2e1498c34ded28db08df6df22b04137afcb
Sukhendra <developer.sukhendra@gmail.com>
2026-09-07 17:48:25 +0530

    rate limiter added for products route

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -20,5 +20,5 @@ export const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a
 
 export const PRODUCT_CACHE_TTL = 60;
 
-export const RATE_LIMIT_WINDOW_MS = 30 * 1000; // 30 seconds
+export const RATE_LIMIT_WINDOW_MS = 30 // 30 seconds
 export const RATE_LIMIT_MAX_REQUESTS = 5;
